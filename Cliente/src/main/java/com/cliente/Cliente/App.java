27efadd54ef872aef0fc2386b4d677b96138ac35
Felipe Xavier <felipexavierass@gmail.com>
2020-11-26 19:09:07 -0300

    ..

--- Cliente/src/main/java/com/cliente/Cliente/App.java
+++ Cliente/src/main/java/com/cliente/Cliente/App.java
@@ -1,7 +1,6 @@
 package com.cliente.Cliente;
 
 import com.cliente.view.ControllerCliente;
-import com.cliente.view.ControllerVendedor;
 
 /**
  * Hello world!
@@ -10,7 +9,6 @@ import com.cliente.view.ControllerVendedor;
 public class App 
 {
 	public static void main(String[] args) {		
-		new ControllerCliente().execute();
-		new ControllerVendedor().execute();
+		new ControllerCliente().execute();		
 	}
 }
